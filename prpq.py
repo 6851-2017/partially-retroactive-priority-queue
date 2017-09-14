1d2dc1187f1daeb601b22da4615ecb5ebffe90a9
ksedlar <ksedlar@Katharines-MacBook-Pro.local>
2017-09-13 21:27:09 -0400

    implemented a solution that assumes all the necessary BBSTs have already been implemented

---
+++ prpq.py
@@ -0,0 +1,20 @@
+# partially retroactive priority queue
+
+class rpq:
+	def __init__(self, q_now, max_or_min)
+		self.q_now = q_now
+		self.max_or_min = max_or_min
+
+
+	def insert_insert(x, t):
+
+		return
+
+	def insert_delete(t):
+		return
+
+	def delete_insert(operation):
+		return
+
+	def delete_delete(operation):
+		return
